7d4126705a35d236b15bb94b34edbba3eb2c8edb
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-15 00:48:38 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,6 +52,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  until display_card_total > 21
+  welcome
+  initial_round
+  hit?
+  display_card_total
+end 
+end_game
 end
     
